a9be2c1ef22b8a9c557ecc220ac7059c2e17b61a
radithya-mahesa <radithya.syabil.com>
2024-08-30 01:20:10 +0700

    feat: add a feature if a user tries to go to the dashboard without logging in

---
+++ login/dashboard/not-login/not-login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../layout/style/not-login.css">
+    <title>Login Dulu Lah 😹</title>
+    <?php include_once "../../../layout/style/not-login.html" ?>
+    <?php include_once "../../../config/bootstrap.html" ?>
+    <style>
+        p { color: black; }
+    </style>
+</head>
+<body>
+    <?php include_once "../../../layout/build/header.html" ?> 
+    <div class="container">
+        <video controls src="Cat Laughing At You.mp4" class="object-fit-contain" loop></video>
+        <h1>APA COBA 😹</h1>
+        <button class="btn btn-danger" style="margin: 10px 0;">
+            <a href="../../../index.php">Login dulu lah😹</a>
+        </button>
+    </div>
+    <?php include_once "../../../layout/build/footer.html" ?> 
+</body>
+</html>
