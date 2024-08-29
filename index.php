e6ffeecf25808ee27557befed104972633d812c4
radithya-mahesa <radithya.syabil.com>
2024-08-30 01:27:34 +0700

    feat: add index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./layout/style/style.css">
+    <title>RRRRRRADIT🦅🔥</title>
+</head>
+<body>
+    <?php include_once "layout/build/header.html" ?>
+    <div class="jumbotron">
+        <h1>SELAMAT DATANG DI WEBSITE SIGMA</h1>
+    </div>
+    <div class="container">
+        <button>
+            <a href="./login/login.php">Bang.. Aku mau login 🗣&#xFE0F;</a>
+        </button>
+        <button>
+            <a href="./register/register.php">Bang.. Aku mau register 🗣&#xFE0F;</a>
+        </button>
+    </div>
+    <?php include_once "layout/build/footer.html" ?>
+</body>
+</html>
